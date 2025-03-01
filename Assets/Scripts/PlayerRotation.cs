d4891b298027aafdf6fe90af673ee5cd3bd60d34
Zytherion <aleks_zlat@abv.bg>
2025-03-01 15:04:26 +0200

    Player jump

--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 public class PlayerRotation : MonoBehaviour
 {
     private Vector2 turn;
+    public float jumpForce = 5f;         // Force of the jump
+    private Rigidbody rb;                // Reference to Rigidbody
+    private bool isGrounded = true;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
     }
 
     // Update is called once per frame
@@ -20,5 +25,23 @@ public class PlayerRotation : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(-turn.y, 0, 0);
 
+        // Check for jump input (space key) and if the character is grounded
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.isKinematic = false;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);  // Apply jump force
+            isGrounded = false;  // Character is in the air
+        }    
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;  // Character is on the ground
+            rb.linearVelocity = Vector3.zero;
+            rb.isKinematic = true;
+        }
     }
 }
