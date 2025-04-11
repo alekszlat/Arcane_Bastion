0181a1d4be8a3211f598999d511e09f4b3e296a6
boqn54545 <boqn54545@gmail.com>
2025-04-11 18:03:37 +0300

    ui fix?

--- Assets/Scripts/PlayerScripts/PlayerUiManager.cs
+++ Assets/Scripts/PlayerScripts/PlayerUiManager.cs
@@ -20,13 +20,25 @@ public class PlayerUiManager : MonoBehaviour
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start()
-    {
-        playerController = GameObject.FindGameObjectWithTag("Player")?.GetComponent<PlayerController>();
-        waveSystemController = GameObject.FindGameObjectWithTag("WaveSystemV2")?.GetComponent<WaveSystemV2>();
-        towerController = GameObject.FindGameObjectWithTag("TowerBase")?.GetComponent<TowerBehaviour>();
-    }
    
+    void OnValidate()
+    {
+        if (playerController == null)
+        {
+            playerController = GameObject.FindGameObjectWithTag("Player")?.GetComponent<PlayerController>();
+        }
+
+        if (waveSystemController == null)
+        { 
+            waveSystemController = GameObject.FindGameObjectWithTag("WaveSystemV2")?.GetComponent<WaveSystemV2>(); 
+        }
+
+        if (towerController == null)
+        {
+            towerController = GameObject.FindGameObjectWithTag("TowerBase")?.GetComponent<TowerBehaviour>();
+        }
+ 
+    }
 
     // Update is called once per frame
     void Update()
