60bb2fee428445a9e018dbd86c0a85fcc0e2d657
Zytherion <alexzlat2005724@gmail.com>
2025-05-19 21:07:52 +0300

    Adding model
    Added the model for the Troll

--- Assets/Scripts/EnemiesScripts/TrollEnemy.cs
+++ Assets/Scripts/EnemiesScripts/TrollEnemy.cs
@@ -2,6 +2,41 @@ using UnityEngine;
 
 public class TrollEnemy : EnemyBehaviour//extends basic enemy behavior
 {
-    
-   
+    [SerializeField] GameObject TrollBase;
+    Animator animator;
+
+    protected override void Awake()
+    {
+        base.Awake(); // Call the base class Awake method
+        animator = TrollBase.GetComponent<Animator>(); // Get the Animator component from the goblin prefab
+    }
+
+    // Override the Update method to fix CS0114
+    public override void Update()
+    {
+        base.Update(); // Call the base class Update method
+        AnimationController(); // Add meaningful logic to avoid UNT0001
+    }
+
+
+    void AnimationController()
+    {
+        if (isHitAnimation)
+        {
+            animator.SetBool("isHitting", false);
+            animator.SetBool("isWalking", true);
+            // Unable the animator
+            animator.enabled = false;
+        }
+        else
+        {
+            animator.enabled = true; // Enable the animator
+        }
+
+        if (isAttackAnimation)
+        {
+            animator.SetBool("isHitting", true); // Trigger the attack animation
+        }
+    }
+
 }
